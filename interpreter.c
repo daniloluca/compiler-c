07f86c20f1da574ff160c7c9c4c44cb9d8a7232f
daniloluca <danilolucasmd@gmail.com>
2016-01-12 22:09:05 -0200

    Adicionados arquivos ao repositorio.

---
+++ interpreter.c
@@ -0,0 +1,306 @@
+ /*
+The Cradle - O Berço
+
+O código abaixo foi escrito por Felipo Soranz e é uma adaptação
+do código original em Pascal escrito por Jack W. Crenshaw em sua
+série "Let's Build a Compiler".
+
+Este código é de livre distribuição e uso.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+char look; /* O caracter lido "antecipadamente" (lookahead) */
+
+/* protótipos */
+void init();
+
+void nextChar();
+void error(char *fmt, ...);
+
+void fatal(char *fmt, ...);
+void expected(char *fmt, ...);
+
+void match(char c);
+char getName();
+
+int getNum();
+void emit(char *fmt, ...);
+
+void term();
+int expression();
+void add();
+void subtract();
+void multiply();
+void divide();
+void factor();
+void ident();
+int isAddOp(char c);
+void assignment();
+
+/* PROGRAMA PRINCIPAL */
+int main(){
+        init();
+        assignment();
+        
+        if (look != '\n')
+            expected("NewLine");
+            
+        printf("%d\n", expression());
+
+        return 0;
+}
+
+/* inicialização do compilador */
+void init(){
+        nextChar();
+}
+
+/* lê próximo caracter da entrada */
+void nextChar(){
+        look = getchar();
+}
+
+/* exibe uma mensagem de erro formatada */
+
+void error(char *fmt, ...){
+        va_list args;
+
+        fputs("Error: ", stderr);
+
+        va_start(args, fmt);
+
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+
+        fputc('\n', stderr);
+}
+
+/* exibe uma mensagem de erro formatada e sai */
+
+void fatal(char *fmt, ...){
+        va_list args;
+
+        fputs("Error: ", stderr);
+
+        va_start(args, fmt);
+
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+
+        fputc('\n', stderr);
+
+        exit(1);
+
+}
+
+/* alerta sobre alguma entrada esperada */
+void expected(char *fmt, ...){
+
+        va_list args;
+
+        fputs("Error: ", stderr);
+
+        va_start(args, fmt);
+        vfprintf(stderr, fmt, args);
+
+        va_end(args);
+
+        fputs(" expected!\n", stderr);
+
+        exit(1);
+}
+
+/* verifica se entrada combina com o esperado */
+void match(char c){
+        if (look != c)
+                expected("'%c'", c);
+
+        nextChar();
+}
+
+/* recebe o nome de um identificador */
+char getName(){
+        char name;
+
+        if (!isalpha(look))
+
+                expected("Name");
+        name = toupper(look);
+
+        nextChar();
+
+        return name;
+}
+
+/* recebe um número inteiro */
+
+int getNum(){
+        char num;
+        
+        if (!isdigit(look))
+
+                expected("Integer");
+        num = look;
+        nextChar();
+
+        return num - '0';
+}
+
+/* emite uma instrução seguida por uma nova linha */
+void emit(char *fmt, ...){
+        va_list args;
+
+        putchar('\t');
+
+        va_start(args, fmt);
+        vprintf(fmt, args);
+
+        va_end(args);
+
+        putchar('\n');
+
+}
+
+/* analisa e traduz uma expressão */
+void term(){
+        factor();
+        while (look == '*' || look == '/') {
+        
+                emit("PUSH AX");
+                switch(look) {
+                  case '*':
+        
+                        multiply();
+                        break;
+                  case '/':
+                        divide();
+        
+                        break;
+                  default:
+                        break;
+        
+                }
+        }
+
+}
+
+ /* reconhece e traduz uma expressão */
+int expression(){
+        
+         int val;
+        
+        if (isAddOp(look))
+        
+                val = 0;
+        else
+                val = getNum();
+        
+        while (isAddOp(look)) {
+                switch (look) {
+        
+                  case '+':
+                        match('+');
+                        val += getNum();
+        
+                        break;
+                  case '-':
+                        match('-');
+        
+                        val -= getNum();
+                        break;
+                }
+        
+        }
+        
+
+}
+
+/* reconhece e traduz uma adição */
+void add(){
+        match('+');
+        term();
+        emit("POP BX");
+        
+        emit("ADD AX, BX");
+}
+
+/* reconhece e traduz uma subtração */
+void subtract(){
+        match('-');
+
+        term();
+        emit("POP BX");
+        
+        emit("SUB AX, BX");
+        emit("NEG AX");
+}
+
+/* analisa e traduz um fator matemático */
+void factor(){
+        if (look == '(') {
+        
+                match('(');
+                expression();
+                match(')');
+
+        } else if(isalpha(look))
+                ident();
+        else
+                emit("MOV AX, %c", getNum());
+}
+
+/* reconhece e traduz uma multiplicação */
+
+void multiply(){
+        match('*');
+        factor();
+
+        emit("POP BX");
+        emit("IMUL BX");
+}
+
+/* reconhece e traduz uma divisão */
+void divide(){
+        match('/');
+
+        factor();
+        emit("POP BX");
+        emit("XCHG AX, BX");
+
+        emit("CWD");
+        emit("IDIV BX");
+}
+
+/* analisa e traduz um identificador */
+void ident(){
+        char name;
+
+        name = getName();
+        if (look == '(') {
+
+                match('(');
+                match(')');
+                emit("CALL %c", name);
+
+        } else
+                emit("MOV AX, [%c]", name);
+}
+
+int isAddOp(char c){
+        return (c=='+' || c=='-');
+}
+
+/* analisa e traduz um comando de atribuição */
+void assignment(){
+        char name;
+
+        name = getName();
+
+        match('=');
+        expression();
+        emit("MOV [%c], AX", name);
+
+}
